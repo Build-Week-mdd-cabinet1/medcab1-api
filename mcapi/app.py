c6735c46f9f3d5f2938696e46289595c9ab79dda
RCTom168 <38849608+RCTom168@users.noreply.github.com>
2020-03-06 13:08:39 -0800

    Update app.py
    Removed unnecessary text

--- mcapi/app.py
+++ mcapi/app.py
@@ -15,7 +15,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def create_app():
     app = Flask(__name__)
 
-    # TODO: get database initialized and migrated
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
